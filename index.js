a9441293fd65293e6c5664a6fe7f33d72fbe2923
Pukar Singh <spukar133@gmail.com>
2023-08-27 14:14:20 +0545

    WOrking on send message

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const express = require("express");
 const app = express();
 const routes = require("./routes/index");
 
+// Serve static files from the "public" directory
 app.use(express.static("public"));
 
+// Set view engine and views directory
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+// Parse JSON bodies
 app.use(express.json());
 
+// Use routes from the "./routes/index" module
 app.use("/", routes);
 
-const PORT = process.env.PORT || 3000;
+// Start the server
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const nodemailer = require("nodemailer");
 
 router.get("/", (req, res) => {
   res.render("index");
@@ -17,14 +18,34 @@ router.get("/service", (req, res) => {
   res.render("service");
 });
 
-router.get("/:name", (req, res) => {
-  const { name } = req.params;
-  res.render("index", { name: name ? name : "" });
-});
+router.post("/mail", async (req, res) => {
+  try {
+    console.log(req);
+    const { name, email, subject, message } = req.body;
 
-router.post("/", (req, res) => {
-  console.log("Received POST request");
-  res.send("Hello POST World");
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      auth: {
+        user: "aaliyah.denesik@ethereal.email",
+        pass: "BRAjhbe5fNREgZp7mt",
+      },
+    });
+
+    const mailOptions = {
+      from: email,
+      to: "myassa92@gmail.com",
+      subject: subject,
+      text: `From: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: " + info.response);
+    res.send("Email sent successfully!");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error sending email.");
+  }
 });
 
 module.exports = router;
